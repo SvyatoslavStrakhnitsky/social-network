bda1b71dfe9e40ba8b0f655058b603f6ba92e38a
SvyatoslavStrakhnitsky <sviatoyyy@gmail.com>
2021-06-05 15:30:29 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://static4.depositphotos.com/1020822/350/v/600/depositphotos_3501921-stock-illustration-photography-eye-logo.jpg"/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://lh3.googleusercontent.com/proxy/Jcfywfb9g0NuJqCRRoyRHzVy2A4Vrk4lk4_izEPF4ILSZPd0mxwfrNLypaXW_LKHHT3LjbTGrXsWkFuq6fTdD9XXIJUgW6w1Z-C2-JYrOPw"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
